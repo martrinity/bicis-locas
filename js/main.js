15a6c82abd87834e983f830e570e7efd48a5b06e
martrinity <martiloca_4@hotmail.com>
2017-05-16 03:27:44 -0400

    probando funciones en browser

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 function validateForm(){
-   var emailValido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-   var soloLetras = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
-
+   
    var nombre = document.getElementById("name").value;
    var apellido = document.getElementById("lastname").value;
    var correo = document.getElementById("input-email").value;
    var pass = document.getElementById("input-password").value;
-   var seleccion = document.getElementsByClassName("form-control").value;
 
-if(Validador.soloTexto(nombre)){
-   quitarInfo("name");
-}else{
-   alertInfo("name", "Nombre no es válido");
+   if(Validador.soloTexto(nombre)){
+      quitarInfo("name");
+   }else{
+      alertInfo("name", "Nombre no es válido");
+   }
+   if(Validador.soloTexto(apellido)){
+      quitarInfo("lastname");
+   }else{
+      alertInfo("lastname", "Ingresa un apellido válido");
+   }
+
+   if(Validador.passValida(pass)){
+      quitarInfo("input-password");
+   }else{
+      alertInfo("input-password", "Debes ingresar contraseña válida");
+   }
+
+   if(Validador.esCorreo(correo)){
+      quitarInfo("input-email");
+   }else{
+      alertInfo("input-email", "Debes ingresar correo válido");
+   }
 }
-if(Validador.soloTexto(apellido)){
-   quitarInfo("apellido");
-}else{
-   alertInfo("apellido", "Ingresa un apellido válido");
-}
-
-
 
 function quitarInfo(idInput){
    var el =document.getElementById(idInput);
@@ -36,13 +44,33 @@ function alertInfo(idInput, info){
       el.parentNode.appendChild(span);
    }else if(el.nextSibling.tagName =="SPAN"){
       el.nextSibling.innerHTML = info;
-      }else{
-        el.parentNode.removeChild(el.nextSibling);
+   }else{
+      el.parentNode.removeChild(el.nextSibling);
 
-      var span = document.createElement("span");
-      span.innerHTML = info;
-      el.parentNode.appendChild(span);
+   var span = document.createElement("span");
+   span.innerHTML = info;
+   el.parentNode.appendChild(span);
       }
-
    }
 }
+
+var Validador = {
+   soloTexto:function (string){
+      var digitos = /([0-9]+)/;
+      return !digitos.test(string);
+   },
+
+   passValida: function (string){
+      var esValida = true;
+      if(string.length < 6 || string == "098754" || string == "123456" || string == "password")
+         esValida = false;
+
+      return esValida;
+   },
+
+   esCorreo:function (string){
+      var especiales = /([a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]+)/g;
+      return especiales.test(string);
+   }
+   
+};
